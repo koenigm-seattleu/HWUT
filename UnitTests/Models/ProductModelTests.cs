0605e3955dfe3a76a199d56ff4599ba43bdcf078
Mike Koenig <koenigm@seattleu.edu>
2020-05-26 14:35:53 -0700

    Added Get Date test to ProductModelTests

--- UnitTests/Models/ProductModelTests.cs
+++ UnitTests/Models/ProductModelTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using HWUT.Models;
+using System;
 
 namespace UnitTests
 {
@@ -16,6 +17,18 @@ namespace UnitTests
 
             // Assert
             Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void ProductModel_Get_Date_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new ProductModel();
+
+            // Assert
+            Assert.AreEqual(DateTime.UtcNow.ToShortDateString(), result.Date.ToShortDateString());
         }
     }
 }
