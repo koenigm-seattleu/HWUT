5015cac7ecf563397248c750739b72f96982709b
Mike Koenig <koenigm@seattleu.edu>
2020-05-26 14:28:08 -0700

    Added ProductModelTests.cs to UT Project

---
+++ UnitTests/Models/ProductModelTests.cs
@@ -0,0 +1,21 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HWUT.Models;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ProductModelTests
+    {
+        [TestMethod]
+        public void ProductModel_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new ProductModel();
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
