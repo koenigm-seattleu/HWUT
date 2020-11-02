5189126ff9baf3eb5b00124c1605c176b961ec6e
koenigm-seattleu <koenigm@seattleu.edu>
2020-11-01 21:56:02 -0800

    Added to project PrivacyTests.cs

---
+++ UnitTests/Pages/PrivacyTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Moq;
+
+using HWUT.Pages;
+
+namespace UnitTests.Pages
+{
+    [TestClass]
+    public class PrivacyTests
+    {
+        [TestMethod]
+        public void Privacy_OnGet_Valid_Should_Return_IsValid()
+        {
+            // Arrange
+            var logger = Mock.Of<ILogger<PrivacyModel>>();
+            var pageModel = new PrivacyModel(logger);
+
+            // Act
+            pageModel.OnGet();
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+        }
+    }
+}
