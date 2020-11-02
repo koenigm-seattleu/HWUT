89daf01204247be9424ed5a0a58f21244e801f66
koenigm-seattleu <koenigm@seattleu.edu>
2020-11-01 21:55:52 -0800

    Added to project IndexTests.cs

---
+++ UnitTests/Pages/IndexTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Moq;
+
+using HWUT.Pages;
+using HWUT.Services;
+
+namespace UnitTests.Pages
+{
+    [TestClass]
+    public class IndexTests
+    {
+        [TestMethod]
+        public void IndexMethod_Valid_Should_Return_Products()
+        {
+            // Arrange
+
+            var dataPath = @"..\\..\\..\\..\\HWUT\\wwwroot";
+
+            var hostEnvironment = new Mock<IWebHostEnvironment>();
+            hostEnvironment
+                .Setup(m => m.WebRootPath)
+                .Returns(dataPath);
+
+            hostEnvironment
+                .Setup(m => m.EnvironmentName)
+                .Returns("Hosting:UnitTestEnvironment");
+
+            var logger = Mock.Of<ILogger<IndexModel>>();
+            var productService = new JsonFileProductService(hostEnvironment.Object);
+            var pageModel = new IndexModel(logger,productService);
+
+            // Act
+            pageModel.OnGet();
+
+            // Assert
+            Assert.AreEqual(6, pageModel.Products.ToList().Count);
+        }
+    }
+}
